0d1c1ec2d8d4fcde7860e501c39886b0abe4fa63
Samhan-6 <msmsamhan844@gmail.com>
2024-06-09 21:30:39 +0530

    update login signup page

--- gemsite/src/components/loginsignup/LoginSignup.jsx
+++ gemsite/src/components/loginsignup/LoginSignup.jsx
@@ -1,25 +1,126 @@
+import { useState } from 'react'
 import './LoginSignup.css'
 
 const LoginSignup = () => {
+  const [state, setState] = useState('Sign In')
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    email: '',
+  })
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const login = async () => {
+    console.log('Login Function Executed', formData)
+    let responseData
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data))
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token)
+      window.location.replace('/')
+    } else {
+      alert(responseData.errors)
+    }
+  }
+
+  const signup = async () => {
+    console.log('Signup Function Executed', formData)
+    let responseData
+    await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data))
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token)
+      window.location.replace('/')
+    } else {
+      alert(responseData.errors)
+    }
+  }
+
   return (
     <div className='loginsignup'>
       <div className='loginsignup-container'>
-        <h1>Sign Up</h1>
+        <div className='loginsignup-heading'>
+          <h1>{state}</h1>
+        </div>
         <div className='loginsignup-fields'>
-          <input type='text' placeholder='Enter Your Name' />
-          <input type='email' placeholder='Enter Your Email Address' />
-          <input type='password' placeholder='Enter Your Password' />
+          {state === 'Sign Up' ? (
+            <input
+              name='username'
+              value={formData.username}
+              onChange={changeHandler}
+              type='text'
+              placeholder='Enter Your Name'
+            />
+          ) : (
+            <></>
+          )}
+          <input
+            name='email'
+            value={formData.email}
+            onChange={changeHandler}
+            type='email'
+            placeholder='Enter Your Email Address'
+          />
+          <input
+            name='password'
+            value={formData.password}
+            onChange={changeHandler}
+            type='password'
+            placeholder='Enter Your Password'
+          />
         </div>
 
-        <button>Continue</button>
+        <div className='login-signup-btn-link'>
+          <button
+            onClick={() => {
+              state === 'Sign In' ? login() : signup()
+            }}>
+            Continue
+          </button>
 
-        <p className='loginsignup-login'>
-          Already have an account? <span>Login Here</span>
-        </p>
-
-        <div className='loginsignup-agree'>
-          <input type='checkbox' name='' id='' />
-          <p>By Continuing, I Agree to The Terms of Services</p>
+          {state === 'Sign Up' ? (
+            <p>
+              Already have an account?{' '}
+              <span
+                onClick={() => {
+                  setState('Sign In')
+                }}>
+                Login Here
+              </span>
+            </p>
+          ) : (
+            <p>
+              Create an Account?{' '}
+              <span
+                onClick={() => {
+                  setState('Sign Up')
+                }}>
+                Click here
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>
