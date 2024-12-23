5755747f411971cdc1afc8fb7a301a628c53833b
Samhan-6 <msmsamhan844@gmail.com>
2024-12-23 12:36:05 +0530

    read config files, connect db, handle unhandle rejections are made successfully

--- backend/server.js
+++ backend/server.js
@@ -1,75 +1,40 @@
-// to run --> npm run dev
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const colors = require('colors')
 
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const colors = require('colors');
-const morgan = require('morgan');
-const fileUpload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
-const errorHandler = require('./middleware/error');
+// read config file
+dotenv.config({ path: './config/config.env' })
 
-// load env vars
-dotenv.config({ path: './config/config.env' });
+// then we should require app
+const app = require('./app')
 
-// Databse connection
-connectDB();
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD)
 
-// route file
-const products = require('./routes/products');
-const auth = require('./routes/auth');
-const users = require('./routes/users');
-const reviews = require('./routes/reviews');
-
-// initialize app variable with express()
-const app = express();
-
-// body parser
-app.use(express.json());
-
-// cookie parser
-app.use(cookieParser());
-
-// dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// file upload
-app.use(fileUpload());
-
-app.use(cors());
-
-// use static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Mount routes
-app.use('/api/v1/products', products);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/users', users);
-app.use('/api/v1/reviews', reviews);
-
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 4000;
-
-// in order to run server we need to call .listen()
-// and then we can also make console.log() to show port info in the terminal
-const server = app.listen(
-  PORT,
+mongoose.connect(DB).then((con) => {
   console.log(
-    `Server Running on the ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-      .bold,
-  ),
-);
+    `DB connected successfully : ${con.connection.host}`.cyan.underline.bold,
+  )
+})
 
-// handle unhandled rejection
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-  // close the server & exit
+const port = process.env.PORT || 4000
+
+const server = app.listen(port, () => {
+  console.log(`server is running on port ${port}...`.blue.bold)
+})
+
+// finally: handling unhandled rejection
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message)
+
   server.close(() => {
-    process.exit(1);
-  });
-});
+    process.exit(1)
+  })
+})
+
+// // file upload
+// app.use(fileUpload())
+
+// app.use(cors())
+
+// // use static folder
+// app.use(express.static(path.join(__dirname, 'public')))
