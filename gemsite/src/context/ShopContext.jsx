22546dde6253cf85ffa3b609cd79c83d062f5a48
Samhan-6 <msmsamhan844@gmail.com>
2025-01-31 17:47:43 +0530

    comment the code, because something went wrong

--- gemsite/src/context/ShopContext.jsx
+++ gemsite/src/context/ShopContext.jsx
@@ -1,28 +1,20 @@
 import { createContext, useEffect, useState } from 'react'
 
-import axiosInstance from '../api/axiosInstance'
-
-export const ShopContext = createContext(null)
+// export const ShopContext = createContext(null)
 
 const ShopContextProvider = (props) => {
-  const [all_product, setAll_Product] = useState([])
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axiosInstance.get('/products')
-        setAll_Product(response.data)
-      } catch (error) {
-        console.error('Error fetching products:', error)
-      }
-    }
-
-    fetchProducts()
-  }, [])
-
-  const contextValue = { all_product }
-
-  return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
+  // const [all_product, setAll_Product] = useState([])
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     try {
+  //     } catch (error) {
+  //       console.error('Error fetching products:', error)
+  //     }
+  //   }
+  //   fetchProducts()
+  // }, [])
+  // const contextValue = { all_product }
+  // return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
 }
 
 export default ShopContextProvider
